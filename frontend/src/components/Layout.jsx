e7b4b7abdd5ea00959dd9235d3114d5ea26567ba
steve-ongera <gadafisteve001@gmail.com>
2026-05-29 02:46:53 +0300

    made updates

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar  from "./Navbar";
+import Sidebar from "./Sidebar";
+
+export default function Layout() {
+  return (
+    <div className="app-layout">
+      <Navbar />
+      <div className="main-content">
+        <Sidebar />
+        <main className="page-content">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
